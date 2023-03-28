771e417a483051f289825b714638263712167891
Alain Herve <alain.1337dev@outlook.com>
2023-03-28 22:21:01 +0200

    init

---
+++ Private/Resolve-FilePath.ps1
@@ -0,0 +1,68 @@
+﻿function Resolve-FilePath {
+    <#
+    .SYNOPSIS
+        Resolve FilePath
+    .DESCRIPTION
+        Gets the full Path of any file in a repo
+    .NOTES
+        Only Created to work with the "devHelper.PsImport" module. (Its not tested for other use cases)
+    .LINK
+        https://github.com/alainQtec/devHelper.PsImport/blob/main/Private/Resolve-FilePath.ps1
+    .EXAMPLE
+        Resolve-FilePath *
+        will list fullNames of files in current location; thus [psimport]::ParseFile("*") will parse any powershell file in current location.
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'singlePath')]
+    param (
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'singlePath')]
+        [ValidateNotNullOrEmpty()][string]$Path,
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true, ParameterSetName = 'multiplePaths')]
+        [ValidateNotNullOrEmpty()][string[]]$FilePaths,
+        [switch]$throwOnFailure = $false,
+        [switch]$NoAmbiguous
+    )
+
+    begin {
+        $paths = @(); $resolved = @();
+        $paths += if ($PSCmdlet.ParameterSetName.Equals('singlePath')) { @($Path) } else { $FilePaths }
+        $rRoot = if (Get-Command -Name git -CommandType Application -ErrorAction Ignore) { git rev-parse --show-toplevel }else { $null }
+    }
+    process {
+        forEach ($p in $paths) {
+            if ($p.Contains('*')) {
+                $(Get-Item $p).Where({ $_.Attributes.ToString() -ne 'Directory' }).FullName.Foreach({ $resolved += $_ })
+                continue
+            }
+            $resolvedPaths = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($p);
+            $existingPaths = $resolvedPaths | Where-Object { [IO.Path]::Exists($_) };
+            $resolvedPaths, $error_Msg = switch ($true) {
+                ($existingPaths.Count -gt 1) {
+                    if ($NoAmbiguous) { $existingPaths[0], "Path '$p' is ambiguous: $($existingPaths -join ', ')" } else { $existingPaths, $null };
+                    break
+                }
+                ($existingPaths.Count -eq 0) {
+                    $fp = $null; if ([IO.Path]::Exists($rRoot)) {
+                        $ft = if (![IO.Path]::HasExtension($p)) { $p + '*' } else { $p }
+                        $fp = Get-ChildItem -Recurse -Path $rRoot -Filter $ft -File -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
+                    }
+                    if (!$fp) { $null, "Path '$p' not found" } else { $fp, $null };
+                    break
+                }
+                Default { $existingPaths, $null }
+            }; if ($error_Msg) { if ($throwOnFailure) { throw [System.Management.Automation.ItemNotFoundException]::new($error_Msg) } else { Write-Warning -Message $error_Msg } }
+            $resolvedPaths.Foreach({ $resolved += $_ })
+        }
+        if ($resolved.Count -gt 1 -and $NoAmbiguous) {
+            $error_Msg = 'Resolved to Multiple paths'
+            if ($throwOnFailure) {
+                throw $error_Msg
+            } else {
+                Write-Warning -Message $error_Msg
+            }
+        }
+    }
+
+    end {
+        return $resolved
+    }
+}
---
+++ Public/Import-Function.ps1
@@ -0,0 +1,66 @@
+﻿function Import-Function {
+    <#
+    .SYNOPSIS
+        Import functions from other scripts into the current script.
+    .DESCRIPTION
+        Close to javascript's ESmodule import functionality.
+        Features:
+        + can import many functions at once
+        + Support wildcards. See examples.
+        + No need to know relative paths of files in the repo. just use unique filename
+    .NOTES
+        Caution: Not tested well.
+        Inspiration: https://gist.github.com/alainQtec/71123a1d28f37eaa49fd032ba0248650
+    .LINK
+        https://github.com/alainQtec/devHelper.PsImport/blob/main/Public/Import-Function.ps1
+    .EXAMPLE
+        Import fnName1, fnName2 -From '/relative/path/to/script.ps1'
+        # Imports the functions fnName1 fnName2
+
+    .EXAMPLE
+        Import * -from '/relative/path/to/script.ps1'
+        # Using wildcards for names: All functions in the file get loaded in current script scope.
+
+        Import * -from '/relative/path/to/fileNamedlikeabc*.ps1'
+        # Import all functions in files that look kike ileNamedlikeabc
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'Names')]
+    [Alias("Import", "require")]
+    param (
+        # Names of functions to import
+        [Parameter(Position = 0, Mandatory = $true, ParameterSetName = 'Names')]
+        [ValidateNotNullOrEmpty()]
+        [Alias('functions')]
+        [string[]]$Names,
+
+        # Names of functions to import
+        [Parameter(Position = 0, Mandatory = $true, ParameterSetName = 'Name')]
+        [ValidateNotNullOrEmpty()]
+        [Alias('n', 'function')]
+        [string]$Name,
+
+        # FilePath from which to import
+        [Parameter(Position = 1, Mandatory = $true, ParameterSetName = '__AllParameterSets')]
+        [ValidateNotNullOrEmpty()]
+        [Alias('f', 'From')]
+        [string[]]$path,
+
+        # Minimum version of function to import
+        [Parameter(Mandatory = $false, Position = 1, ParameterSetName = 'Name')]
+        [ValidateNotNull()][Alias('MinVersion')]
+        [version]$Version
+    )
+
+    begin {
+        [string[]]$fnNames = if ($PSCmdlet.ParameterSetName -eq 'Name') { $Name } else { $Names }
+        $functions = @(); if ($path.Count -eq 1) { [string]$path = $path[0] }
+    }
+    process {
+        foreach ($n in $fnNames) {
+            $functions += [PsImport]::GetFunction($n, $path)
+        }
+        foreach ($function in $functions) {
+            . $function.scriptblock
+        }
+    }
+}
