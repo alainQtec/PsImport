6fd02d471bc1c7600198026bf12d60927e5e0040
Alain Herve <alain.1337dev@outlook.com>
2023-03-29 09:35:29 +0200

    Create Test-Module.ps1

---
+++ Test-Module.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.SYNOPSIS
+    Run Tests
+.NOTES
+    It is not included in the module.
+#>
+param (
+    [Parameter(Mandatory = $true, Position = 0)]
+    [Alias('Module')][string]$ModulePath,
+    # Path Containing Tests
+    [Parameter(Mandatory = $true, Position = 1)]
+    [Alias('Tests')][string]$TestsPath
+)
+$manifestPath = Join-Path $ModulePath "devHelper.PsImport.psd1"
+if ([IO.File]::Exists($manifestPath)) {
+    Import-Module $manifestPath
+} else {
+    throw 'Module manifest file Was not Found!'
+}
+Write-Verbose "[+] Running tests ..."
+Test-ModuleManifest -Path $manifestPath -ErrorAction Stop -Verbose
+Invoke-Pester -Path $TestsPath -OutputFormat NUnitXml -OutputFile "$TestsPath\results.xml"
